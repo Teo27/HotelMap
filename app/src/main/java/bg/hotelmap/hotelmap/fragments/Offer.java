12ed1fad92a3615560108fd6c7338d0a9f02c446
Teodor Dimitriev <teodordimitriev@gmail.com>
2016-10-11 00:24:55 +0300

    Can change types now

---
+++ app/src/main/java/bg/hotelmap/hotelmap/fragments/Offer.java
@@ -0,0 +1,27 @@
+package bg.hotelmap.hotelmap.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import bg.hotelmap.hotelmap.R;
+
+/**
+ * Created by Teo on 10-Oct-16.
+ */
+
+public class Offer extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.offer_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
